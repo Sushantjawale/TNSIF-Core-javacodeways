135b3f5153662cfcb81b8358d6bb927496b3eb89
User <User@DESKTOP-3D3KE24>
2025-06-30 12:20:15 +0530

    starting

---
+++ project 2/src/com/javaday1/Day1projects.java
@@ -0,0 +1,12 @@
+package com.javaday1;
+
+public class Day1projects {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int sum = a + b;
+		
+		System.out.println(sum);
+	}
+
+}
